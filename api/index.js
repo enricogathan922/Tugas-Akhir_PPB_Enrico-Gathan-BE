049e4b21444e802441f3ddad0d073586cd5c116b
enricogathan922 <enricogathankuliah@gmail.com>
2025-11-25 16:01:05 +0700

    fix: cors

---
+++ api/index.js
@@ -0,0 +1,2 @@
+import handler from "../dist/index.js";
+export default handler;
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,5 @@ import { App } from "./App";
 const PORT = parseInt(process.env.APP_PORT || "3000", 10);
 const app = new App(PORT);
 
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen();
-}
-
 export const handler = app.getApp();
 export default handler;
