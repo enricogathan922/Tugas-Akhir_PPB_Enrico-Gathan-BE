bb35f4a1b3fc628a2f25f749fde82a1fdec17ea5
enricogathan922 <enricogathankuliah@gmail.com>
2025-11-25 16:05:17 +0700

    fix: vercel.json

--- api/index.js
+++
@@ -1,2 +0,0 @@
-import handler from "../dist/index.js";
-export default handler;
--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,4 @@ import { App } from "./App";
 const PORT = parseInt(process.env.APP_PORT || "3000", 10);
 const app = new App(PORT);
 
-export const handler = app.getApp();
-export default handler;
+app.listen();
